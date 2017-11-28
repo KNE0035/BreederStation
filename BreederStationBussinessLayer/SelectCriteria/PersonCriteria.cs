f74db09dce8c289156b63248f2d5f3f9df18d8c7
Marek <marek.kneys.st@vsb.cz>
2017-11-28 17:05:24 +0100

    final changes

---
+++ BreederStationBussinessLayer/SelectCriteria/PersonCriteria.cs
@@ -0,0 +1,10 @@
+﻿using BreederStationBussinessLayer.Domain.Enums;
+
+namespace BreederStationBussinessLayer.SelectCriteria
+{
+    public class PersonCriteria
+    {
+        public RoleEnum? Role { get; set; }
+        public string WholeName { get; set; }
+    }
+}
---
+++ BreederStationDesktopView/AnimalChoiceItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChovnaStanice.Forms
+{
+    class AnimalChoiceItem
+    {
+        public ComboBox AnimalNameComboBox { get; set; }
+        public TextBox AnimalRace { get; set; }
+        public TextBox AnimalId { get; set; }
+        public TextBox AnimalGroupId { get; set; }
+        public Button MinusButton { get; set; }
+
+    }
+}
